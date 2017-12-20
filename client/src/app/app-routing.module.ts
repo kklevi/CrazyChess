14535f98201ea4d968a529d622f1b655de432b6d
Peter James Bernante <elbernante@gmail.com>
2017-12-20 10:27:00 -0600

    feat: Integrate Chess component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { AuthGuard } from './_guards/auth.guard';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component'; 
+import { ChessComponent } from './chess.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
+  { path: 'chess', component: ChessComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }
 ];
 
