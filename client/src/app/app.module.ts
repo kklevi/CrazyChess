28a6211030ab150938f03d7c905eff067b706fac
Peter James Bernante <elbernante@gmail.com>
2017-12-19 22:37:39 -0600

    feat: Add home and login components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChessComponent } from './chess.component';
 import { ChessService } from './chess.service';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChessComponent
+    ChessComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
